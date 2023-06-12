f6c883a557c52fb0edbebfd8efff9780d1f34a3d
Khomotso <110993313+Khomotso012@users.noreply.github.com>
2023-06-12 15:29:58 +0200

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -2,10 +2,9 @@
 
 /**
  * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
+ * @info: Structure
+ *  Return: exit status
+ *
  */
 int _myexit(info_t *info)
 {
@@ -30,10 +29,10 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * _mycd - current directory
+ * @info: Structure
+ * 
+ *  Return: 0
  */
 int _mycd(info_t *info)
 {
@@ -80,9 +79,9 @@ int _mycd(info_t *info)
 }
 
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * _myhelp - changes the current directory
+ * @info: Structure
+ * 
  *  Return: Always 0
  */
 int _myhelp(info_t *info)
