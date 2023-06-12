c260e3d2ab562da783a980104fba1d118baa119a
Khomotso <110993313+Khomotso012@users.noreply.github.com>
2023-06-12 15:32:14 +0200

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * _myhistory - history list
+ * @info: Structure
+ * 
  *  Return: Always 0
  */
 int _myhistory(info_t *info)
@@ -15,10 +14,10 @@ int _myhistory(info_t *info)
 
 /**
  * unset_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
+ * @info: struct
+ * @str: string
  *
- * Return: Always 0 on success, 1 on error
+ * Return: 0
  */
 int unset_alias(info_t *info, char *str)
 {
@@ -38,10 +37,10 @@ int unset_alias(info_t *info, char *str)
 
 /**
  * set_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
+ * @info: struct
+ * @str: string
  *
- * Return: Always 0 on success, 1 on error
+ * Return: 0
  */
 int set_alias(info_t *info, char *str)
 {
@@ -58,10 +57,10 @@ int set_alias(info_t *info, char *str)
 }
 
 /**
- * print_alias - prints an alias string
- * @node: the alias node
+ * print_alias -alias
+ * @node: alias node
  *
- * Return: Always 0 on success, 1 on error
+ * Return: 0
  */
 int print_alias(list_t *node)
 {
@@ -81,9 +80,9 @@ int print_alias(list_t *node)
 }
 
 /**
- * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * _myalias - alias
+ * @info: Structure
+ * 
  *  Return: Always 0
  */
 int _myalias(info_t *info)
