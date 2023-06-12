ac41e09af7a6f5b3491f1a9554d1c4df9f12ef52
Khomotso <110993313+Khomotso012@users.noreply.github.com>
2023-06-12 15:58:01 +0200

    Update lists1.c

--- lists1.c
+++ lists1.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * list_len - determines length of linked list
- * @h: pointer to first node
+ * list_len - length of list
+ * @h: pointer
  *
  * Return: size of list
  */
@@ -19,8 +19,8 @@ size_t list_len(const list_t *h)
 }
 
 /**
- * list_to_strings - returns an array of strings of the list->str
- * @head: pointer to first node
+ * list_to_strings - r array of strings
+ * @head: pointer
  *
  * Return: array of strings
  */
@@ -56,8 +56,8 @@ char **list_to_strings(list_t *head)
 
 
 /**
- * print_list - prints all elements of a list_t linked list
- * @h: pointer to first node
+ * print_list - prints elements
+ * @h: pointer
  *
  * Return: size of list
  */
@@ -79,10 +79,10 @@ size_t print_list(const list_t *h)
 }
 
 /**
- * node_starts_with - returns node whose string starts with prefix
- * @node: pointer to list head
- * @prefix: string to match
- * @c: the next character after prefix to match
+ * node_starts_with - string starts with prefix
+ * @node: pointer 
+ * @prefix: string
+ * @c: char
  *
  * Return: match node or null
  */
@@ -101,8 +101,8 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 }
 
 /**
- * get_node_index - gets the index of a node
- * @head: pointer to list head
+ * get_node_index -  index of a node
+ * @head: pointer
  * @node: pointer to the node
  *
  * Return: index of node or -1
