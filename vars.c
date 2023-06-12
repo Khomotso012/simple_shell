7b73f2e360b53f5f632ad05b732b4127a45fcdc6
Khomotso <110993313+Khomotso012@users.noreply.github.com>
2023-06-12 15:37:32 +0200

    Update vars.c

--- vars.c
+++ vars.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * is_chain - test if current char in buffer is a chain delimeter
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
+ * is_chain - chain delimeter
+ * @info: struct
+ * @buf: buffer
+ * @p: address
  *
- * Return: 1 if chain delimeter, 0 otherwise
+ * Return: 1
  */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
@@ -36,12 +36,12 @@ int is_chain(info_t *info, char *buf, size_t *p)
 }
 
 /**
- * check_chain - checks we should continue chaining based on last status
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
- * @i: starting position in buf
- * @len: length of buf
+ * check_chain - checks
+ * @info: struct
+ * @buf: char buffer
+ * @p: address
+ * @i: starting position
+ * @len: length
  *
  * Return: Void
  */
@@ -70,8 +70,8 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 }
 
 /**
- * replace_alias - replaces an aliases in the tokenized string
- * @info: the parameter struct
+ * replace_alias - replaces an aliases
+ * @info: struct
  *
  * Return: 1 if replaced, 0 otherwise
  */
@@ -99,8 +99,8 @@ int replace_alias(info_t *info)
 }
 
 /**
- * replace_vars - replaces vars in the tokenized string
- * @info: the parameter struct
+ * replace_vars - replaces vars
+ * @info: struct
  *
  * Return: 1 if replaced, 0 otherwise
  */
@@ -141,7 +141,7 @@ int replace_vars(info_t *info)
 
 /**
  * replace_string - replaces string
- * @old: address of old string
+ * @old: address
  * @new: new string
  *
  * Return: 1 if replaced, 0 otherwise
