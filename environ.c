7113ed8762b4ed2851b98bae25bd39e4a3260cd9
Khomotso <110993313+Khomotso012@users.noreply.github.com>
2023-06-12 15:35:07 +0200

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * _myenv - current environment
+ * @info: Structure
+ * 
+ * Return: 0
  */
 int _myenv(info_t *info)
 {
@@ -13,9 +13,9 @@ int _myenv(info_t *info)
 }
 
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
+ * _getenv - environment variable
+ * @info: Structure
+ * @name: name
  *
  * Return: the value
  */
@@ -35,11 +35,10 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _mysetenv - environment variable
+ * @info: Structure
+ * 
+ * Return: 0
  */
 int _mysetenv(info_t *info)
 {
@@ -54,10 +53,10 @@ int _mysetenv(info_t *info)
 }
 
 /**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _myunsetenv - environment variable
+ * @info: Structure
+ * 
+ * Return: Always 0
  */
 int _myunsetenv(info_t *info)
 {
@@ -75,9 +74,9 @@ int _myunsetenv(info_t *info)
 }
 
 /**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * populate_env_list - environment linked list
+ * @info: Structure
+ * 
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
