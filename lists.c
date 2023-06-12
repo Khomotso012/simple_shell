ac3663b3ab4c55ea63ffb2515c86c70e44a8d75a
Khomotso <110993313+Khomotso012@users.noreply.github.com>
2023-06-12 15:56:12 +0200

    Update lists.c

--- lists.c
+++ lists.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * add_node - adds a node to the start of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
+ * add_node - adds a node
+ * @head: address of pointer
+ * @str: str
+ * @num: node index
  *
  * Return: size of list
  */
@@ -34,10 +34,10 @@ list_t *add_node(list_t **head, const char *str, int num)
 }
 
 /**
- * add_node_end - adds a node to the end of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
+ * add_node_end - adds a node
+ * @head: address of pointer
+ * @str: str field
+ * @num: node index
  *
  * Return: size of list
  */
@@ -75,8 +75,8 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
- * print_list_str - prints only the str element of a list_t linked list
- * @h: pointer to first node
+ * print_list_str - prints str element
+ * @h: pointer
  *
  * Return: size of list
  */
@@ -95,9 +95,9 @@ size_t print_list_str(const list_t *h)
 }
 
 /**
- * delete_node_at_index - deletes node at given index
- * @head: address of pointer to first node
- * @index: index of node to delete
+ * delete_node_at_index - deletes node
+ * @head: address of pointer
+ * @index: node index
  *
  * Return: 1 on success, 0 on failure
  */
@@ -135,8 +135,8 @@ int delete_node_at_index(list_t **head, unsigned int index)
 }
 
 /**
- * free_list - frees all nodes of a list
- * @head_ptr: address of pointer to head node
+ * free_list - frees all nodes
+ * @head_ptr: address of pointer
  *
  * Return: void
  */
