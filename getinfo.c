fa652781d9ca33d02399c4a26cfab66c59b03ead
Khomotso <110993313+Khomotso012@users.noreply.github.com>
2023-06-12 15:52:38 +0200

    Update getinfo.c

--- getinfo.c
+++ getinfo.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * clear_info - initializes info_t struct
- * @info: struct address
+ * clear_info - info_t struct
+ * @info: struct
  */
 void clear_info(info_t *info)
 {
@@ -13,9 +13,9 @@ void clear_info(info_t *info)
 }
 
 /**
- * set_info - initializes info_t struct
- * @info: struct address
- * @av: argument vector
+ * set_info - info_t struct
+ * @info: struct
+ * @av: argument
  */
 void set_info(info_t *info, char **av)
 {
@@ -45,9 +45,9 @@ void set_info(info_t *info, char **av)
 }
 
 /**
- * free_info - frees info_t struct fields
- * @info: struct address
- * @all: true if freeing all fields
+ * free_info - frees info_t struct
+ * @info: struct
+ * @all: all fields
  */
 void free_info(info_t *info, int all)
 {
